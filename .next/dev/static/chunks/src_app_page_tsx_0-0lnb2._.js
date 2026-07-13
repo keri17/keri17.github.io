f3e4b4cdec04abc84057f3daf85a5679c4a5c919
keri17 <kerodginbaru21@gmail.com>
2026-07-13 12:36:36 +0300

    Delete .next/dev directory

--- .next/dev/static/chunks/src_app_page_tsx_0-0lnb2._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_02h8~ru._.js"],
-    source: "dynamic"
-});
